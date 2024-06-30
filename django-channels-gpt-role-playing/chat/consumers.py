adb2abea99d62a6a6d9c39330322f6945f9cd2e3
정승현 <armyso9506@gmail.com>
2024-06-30 22:30:39 +0900

    [ openai ] Feature: GPT 대화 기능 및 추천 기능 구현
    [ openai ] Feature: GPT 대화 기능 및 추천 기능 구현

--- django-channels-gpt-role-playing/chat/consumers.py
+++ django-channels-gpt-role-playing/chat/consumers.py
@@ -11,6 +11,7 @@ class RolePlayingRoomConsumer(JsonWebsocketConsumer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.gpt_messages: List[GptMessage] = []
+        self.recommend_message: str = ""
 
     def connect(self):
         room = self.get_room()
@@ -20,6 +21,7 @@ class RolePlayingRoomConsumer(JsonWebsocketConsumer):
             self.accept()
 
             self.gpt_messages = room.get_initial_messages()
+            self.recommend_message = room.get_recommend_message()
 
             assistant_message = self.get_query()
             self.send_json(
@@ -29,9 +31,30 @@ class RolePlayingRoomConsumer(JsonWebsocketConsumer):
                 }
             )
 
-    def receive_json(self, content, **kwargs):
-        # Echo
-        self.send_json(content)
+    def receive_json(self, content_dict, **kwargs):
+        if content_dict["type"] == "user-message":
+            assistant_message = self.get_query(user_query=content_dict["message"])
+            self.send_json(
+                {
+                    "type": "assistant-message",
+                    "message": assistant_message,
+                }
+            )
+        elif content_dict["type"] == "request-recommend-message":
+            recommended_message = self.get_query(command_query=self.recommend_message)
+            self.send_json(
+                {
+                    "type": "recommended-message",
+                    "message": recommended_message,
+                }
+            )
+        else:
+            self.send_json(
+                {
+                    "type": "error",
+                    "message": f"Invalid type: {content_dict['type']}",
+                }
+            )
 
     def get_room(self) -> RolePlayingRoom | None:
         user: AbstractUser = self.scope["user"]
